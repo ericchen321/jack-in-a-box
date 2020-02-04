ed05db924090fe0699d19c4c3d7da59c4f5ea5a0
ericchen321 <cgx1997@outlook.com>
2020-02-04 11:20:32 -0800

    stub of the main function

--- mainframe/main.c
+++ mainframe/main.c
@@ -5,6 +5,25 @@
  */
 
 void main() {
-    // TODO: stub
+    // TODO: initialization: initialize UART, touchscreen, GUI, etc.
+
+    // TODO: state INIT - display the welcome screen
+
+    // TODO: state DISPLAY_INSTRUCTION - display the game instruction
+
+    // TODO: state SETUP - set up phase of the game
+
+    // TODO: state PLAY_TURN - player's turn
+
+    // TODO: state DEALER_TURN - dealer's turn
+
+    // TODO: state PLAYER_WIN - player has won
+
+    // TODO: state DEALER_WIN - dealer has won
+
+    // TODO: state TIE - tie
+
+    // TODO: state SEND_RESULT - allows user to send result to phone, return to the welcome screen, or start again
+    
     return;
 }
