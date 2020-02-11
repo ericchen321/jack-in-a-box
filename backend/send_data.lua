f76477038dfa6074a874268cc1bc0b132f4f0686
Ben Harder <ben.harder2@gmail.com>
2020-02-11 14:25:51 -0800

    working... finally

--- backend/send_data.lua
+++ backend/send_data.lua
@@ -7,18 +7,20 @@ wifi.sta.autoconnect(1)
 
 tmr.delay(5000000)
 
-HOST = "http://d8zdah4wk5t8c.cloudfront.net"
+HOST = "d8zdah4wk5t8c.cloudfront.net"
 
-sample_data = '{"username": "noob", "game_result": "w", "phone_number": "+17788469337"}'
+sample_data = '{"username": "noob", "game_result": "t", "phone_number": "+17788469337"}'
 
 function build_post_request(host, payload)
-    request = "POST HTTP/1.1\r\n"..
+    request = "POST / HTTP/1.1\r\n"..
     "Host: "..host.."\r\n"..
     "Connection: close\r\n"..
     "Content-Type: application/json\r\n"..
     "Content-Length: "..string.len(payload).."\r\n"..
     "\r\n"..
     payload
+
+    print(request)
     return request
 end
 
@@ -43,6 +45,7 @@ function main()
     if(ip==nil) then 
         print("Connecting...")
     else
+        tmr.create():stop(0)
         print("Connected!")
         print(ip)
         -- Send data
@@ -51,4 +54,10 @@ function main()
 
 end
 
-main()
+-- tmr.create():alarm(5000, tmr.ALARM_SINGLE, main)
+-- tmr.alarm(0,7000,1,main) 
+
+if not tmr.create():alarm(5000, tmr.ALARM_SINGLE, main)
+then
+    print("whoopsie")
+end
