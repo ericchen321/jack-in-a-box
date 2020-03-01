db7b61dc2d9575267cb8b8d3d54750066ea543f0
ericchen321 <cgx1997@outlook.com>
2020-03-01 14:10:37 -0800

    restructured mainframe files for the DS5 project

--- ModuleAComputer/cosine_inst.v
+++
@@ -1,5 +0,0 @@
-cosine	cosine_inst (
-	.clock ( clock_sig ),
-	.data ( data_sig ),
-	.result ( result_sig )
-	);
--- ModuleAComputer/float2int_inst.v
+++
@@ -1,5 +0,0 @@
-float2int	float2int_inst (
-	.clock ( clock_sig ),
-	.dataa ( dataa_sig ),
-	.result ( result_sig )
-	);
